9ae33daad7ee703226cf239c951c001148f6ca28
Harsh Mittal <mittalh863@gmail.com>
2021-10-16 03:44:38 +0530

    :tada: Created ng-wheel-rotate demo application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {WheelRotateAxis, WheelRotateEvent} from "ng-wheel-rotate";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-wheel-rotate-demo';
+
+  wheelRotateControls = {
+    debounce: new FormControl(0),
+    delay: new FormControl(0),
+    disabled: new FormControl(false),
+    wheelRotateAxis: new FormControl(WheelRotateAxis.BOTH),
+  };
+  wheelRotateEvent!: WheelRotateEvent;
+  wheelRotateAxis = WheelRotateAxis;
+
+  onWheelRotate(data: WheelRotateEvent) {
+    this.wheelRotateEvent = data;
+  }
 }
